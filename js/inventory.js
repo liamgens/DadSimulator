39fbf41c6d191a7abbb11923f2ef37f74fa56cf8
Tristan Wiley <tristan@tristanwiley.com>
2018-01-27 17:25:36 -0500

    To upper case

--- js/inventory.js
+++ js/inventory.js
@@ -1,11 +1,11 @@
 function drag(ev) {
-    ev.dataTransfer.setData("text", "beer");
+    ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    itemRecieved(data)
+    itemRecieved(data.toUpperCase())
 }
 
 function allowDrop(ev) {
