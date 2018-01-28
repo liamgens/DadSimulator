069f58724894acdf0072ef78997762380e99b794
Tristan Wiley <tristan@tristanwiley.com>
2018-01-27 22:17:26 -0500

    Some work

--- js/inventory.js
+++ js/inventory.js
@@ -4,21 +4,42 @@ if (!Cookies.get("gameStarted")) {
         name: "beer",
         quantity: 3, 
     }])
+    Cookies.set("totalMoney", 20)
 }
 
+loadShopItems()
 updateInventory()
+
+function loadShopItems() {
+    for (let name in GAME_PRODUCTS) {
+        let item = GAME_PRODUCTS[name]
+        $('#shop-items').append(`
+            <div id="${name}" class="shop-item">
+                <img src="assets/${name}.jpg" />
+                <div class="description">
+                    <span class="name">${name.charAt(0).toUpperCase() + name.slice(1)}</span>
+                    <button onclick="buyItem('${name}')">Buy +1</button>
+                    <br>
+                    <span class="price">Price: $${item.price}</span>
+                    <p>${item.description}</p>
+                </div>
+            </div>
+        `)
+    }
+}
 
 function updateInventory() {
     const allItems = Cookies.getJSON("items")
     for (let item of allItems) {
-        console.log(item)
-        $('#inventoryitems').append(`
+        $('#inventoryitems').html(`
             <div class="item">
                 <img id="${item.name}" class="item" draggable="true" ondragstart="drag(event)" src="assets/${item.name}.jpg">
                 <span id="count">${item.quantity}</span>
             </div>
         `)
     }
+
+    $('#money').text(`$${Cookies.get("totalMoney")}`)
 }
 
 function drag(ev) {
@@ -29,6 +50,8 @@ function drop(ev) {
     ev.preventDefault()
     var data = ev.dataTransfer.getData("text")
     itemRecieved(data.toUpperCase())
+
+    removeItem(data)
 }
 
 function allowDrop(ev) {
@@ -44,12 +67,29 @@ function closePopShop() {
 }
 
 function buyItem(item) {
-    const allItems = Cookies.get("items")
+    const allItems = Cookies.getJSON("items")
     if (allItems) {
-        for (let item of allItems) {
-            console.log(item)
+        let obj = allItems.find((obj) => obj.name === "beer")
+        if (obj) {
+            obj.quantity = obj.quantity + 1
+            Cookies.set("items", allItems)
         }
     }
+
+    updateInventory()
+}
+
+function removeItem(item) {
+    const allItems = Cookies.getJSON("items")
+    if (allItems) {
+        let obj = allItems.find((obj) => obj.name === "beer")
+        if (obj) {
+            obj.quantity = obj.quantity - 1
+            Cookies.set("items", allItems)
+        }
+    }
+
+    updateInventory()
 }
 
 $('#popshop').draggable({ handle: "#popshopnavbar" })
---
+++ js/variables.js
@@ -0,0 +1,32 @@
+const GAME_PRODUCTS = {
+    beer: {
+        description: "A nice, ice cold beer will keep your Dad's thirst quenched and happiness up.",
+        price: 2,
+        stat: {
+            health: 5,
+            happiness: 5,
+            hunger: 0,
+            thirst: 5,
+        },
+    },
+    burger: {
+        description: "A nice, ice cold beer will keep your Dad's thirst quenched and happiness up.",
+        price: 5,
+        stat: {
+            health: 2,
+            happiness: 6,
+            hunger: 5,
+            thirst: 0,
+        },
+    },
+    chicken: {
+        description: "A nice, ice cold beer will keep your Dad's thirst quenched and happiness up.",
+        price: 10,
+        stat: {
+            health: 7,
+            happiness: 4,
+            hunger: 10,
+            thirst: 0,
+        },
+    },
+}
